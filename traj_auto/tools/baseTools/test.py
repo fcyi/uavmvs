fix overlap calculation and add some codes about path smooth and fix sparse based fix camera parameters with depth

traj_auto/tools/baseTools/test.py
@@ -1,9 +1,222 @@
+import os.path
+
 import colmap_read_write_model as crwm
+from plyfile import PlyData, PlyElement
+import numpy as np
+import open3d as o3d
+
+
+def fetchPly(path):
+    plydata = PlyData.read(path)
+    vertices = plydata['vertex']
+    positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
+    colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
+    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    tracks = np.vstack(vertices['track']) if 'track' in vertices else np.zeros((positions.shape[0], 1))
+
+    return
+
+
+def storePly(path, xyz, rgb, tracks):
+    # Define the dtype for the structured array
+    dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
+             ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
+             ('red', 'u1'), ('green', 'u1'), ('blue', 'u1'),
+             ('track', 'u1')]
+
+    normals = np.zeros_like(xyz)
+
+    elements = np.empty(xyz.shape[0], dtype=dtype)
+    attributes = np.concatenate((xyz, normals, rgb, tracks), axis=1)
+    elements[:] = list(map(tuple, attributes))
+
+    # Create the PlyData object and write to file
+    vertex_element = PlyElement.describe(elements, 'vertex')
+    ply_data = PlyData([vertex_element])
+    ply_data.write(path)
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr)  # 注意这里使用 len(arr)
+
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid  # 这里不会跳过可能的目标值
+
+    if left < len(arr) and arr[left] == target:
+        return left  # 找到目标值
+    return -1  # 未找到目标值
+
+
+def mvsPly_to_colBin(path_):
+    plydata = PlyData.read(path_)
+    vertices = plydata['vertex']
+    xyz_ = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
+    rgb_ = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
+    # # points_ = np.hstack([positions, colors])
+    # # outPath_ = path_.rsplit('.', 1)[0] + '.txt'
+    # # np.savetxt(outPath_, points_, fmt="%.6f %.6f %.6f %.6f %.6f %.6f")
+    # xyzRgb_ = np.loadtxt(path_)
+    # xyz_ = xyzRgb_[:, :3]
+    # rgb_ = xyzRgb_[:, 3:6]
+
+    print(xyz_.shape[0])
+    pointCloud_ = o3d.geometry.PointCloud()
+    pointCloud_.points = o3d.utility.Vector3dVector(xyz_)
+    pointCloud_.colors = o3d.utility.Vector3dVector(rgb_)
+
+    downVoxelSizeL_, downVoxelSizeR_ = 0.01, 0.5  # 注意这里使用 len(arr)
+    ptLimitMin_ = 6_000_000
+    ptLimitMax_ = 7_000_000
+    downVoxelSizeM_ = -1
+    canFlg = True
+
+    print("000000000000000000000000000000000000000000000000000")
+
+    while True:
+        pcdDownMin_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeR_)
+        ptNumsMin_ = np.array(pcdDownMin_.points).shape[0]
+        if ptNumsMin_ > ptLimitMax_:
+            if downVoxelSizeR_ < 1.:
+                downVoxelSizeR_ *= 2
+            else:
+                print('point clouds are so more')
+                canFlg = False
+                break
+        else:
+            break
+
+    print("000000000000000000000000000000000000000000000000000")
+
+    while True:
+        pcdDownMax_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeL_)
+        ptNumsMax_ = np.array(pcdDownMax_.points).shape[0]
+        if ptNumsMax_ < ptLimitMin_:
+            if downVoxelSizeL_ > 0.001:
+                downVoxelSizeL_ /= 2.
+            else:
+                print('point clouds are so sparse')
+                canFlg = False
+                break
+        else:
+            break
+
+    if not canFlg:
+        raise Exception
+
+    print("000000000000000000000000000000000000000000000000000")
+
+    while downVoxelSizeL_ < downVoxelSizeR_:
+        downVoxelSizeM_ = (downVoxelSizeL_ + downVoxelSizeR_) / 2.
+        pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeM_)
+        ptNumsT_ = np.array(pcdDown_.points).shape[0]
+        print(downVoxelSizeL_, downVoxelSizeR_, ptNumsT_)
+
+        if ptLimitMin_ <= ptNumsT_ <= ptLimitMax_:
+            break
+
+        if ptNumsT_ < ptLimitMin_:
+            downVoxelSizeR_ = downVoxelSizeM_
+        else:
+            downVoxelSizeL_ = downVoxelSizeM_
+
+    print("000000000000000000000000000000000000000000000000000")
+
+    pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeM_)
+
+    xyz_ = []
+    rgb_ = []
+
+    xyz_ = np.asarray(pcdDown_.points)
+    rgb_ = np.asarray(pcdDown_.colors)
+    if rgb_[0][0] < 1 or rgb_[0][1] < 1 or rgb_[0][2] < 1:
+        rgb_ = (rgb_ * 255).astype(int)
+
+    ptNums_ = xyz_.shape[0]
+    print(ptNums_)
+    points3D_ = {}
+    for ptId_ in range(ptNums_):
+        points3D_[ptId_] = crwm.Point3D(
+            id=ptId_,
+            xyz=xyz_[ptId_, :3],
+            rgb=rgb_[ptId_, :3],
+            error=np.array([0]),
+            image_ids=np.array([]),
+            point2D_idxs=np.array([])
+        )
+    outPath_ = path_.rsplit('.', 1)[0] + '.bin'
+    crwm.write_points3D_binary(points3D_, outPath_)
+
+
+def test_removeIds(inputPath_, removeNames_, outputPath_=None):
+    crwm.remove_images_based_fileNames(inputPath_, removeNames_, outputPath_)
+
+
+def images_clear_tracks(path_):
+    images_ = crwm.read_images_binary(path_)
+    imagesClear_ = {}
+    for iK_ in images_.keys():
+        image_ = images_[iK_]
+        imagesClear_[iK_] = crwm.Image(
+            id=image_.id,
+            qvec=image_.qvec,
+            tvec=image_.tvec,
+            camera_id=image_.camera_id,
+            name=image_.name,
+            xys=np.array([]),
+            point3D_ids=np.array([])
+        )
+
+    outPath_ = path_.rsplit('.', 1)[0] + '_clear.bin'
+    crwm.write_images_binary(imagesClear_, outPath_)
+
+
+def get_mvs_time(timeSL):
+    timeL = [0, 0, 0, 0]
+    for timeS in timeSL:
+        timeE = timeS.split('.')
+        timeET = [float(tm_) for tm_ in timeE]
+        timeET = timeET[::-1]
+        for i in range(len(timeET)):
+            timeL[3-i] += timeET[i]
+
+    step3_ = float(int(timeL[3]) // 1000)
+    timeL[3] = int(timeL[3]) % 1000
+
+    timeL[2] += step3_
+    step2_ = float(int(timeL[2]) // 60)
+    timeL[2] = int(timeL[2]) % 60
+
+    timeL[1] += step2_
+    step1_ = float(int(timeL[1]) // 60)
+    timeL[1] = int(timeL[1]) % 60
+
+    timeL[0] += step1_
+    timeL = [str(tt_) for tt_ in timeL]
+    print(':'.join(timeL))
 
 
 if __name__ == '__main__':
-    inputPath = ''
-    outputPath = ''
-    removeName = []
-
-    crwm.remove_images_based_fileNames(inputPath, outputPath, removeName)
+    # test_removeIds('/home/hongqingde/devdata/map/paper/block2_sxfx_GM/sparse/0/images.bin', removeNames_=['100695'])
+    mvsPly_to_colBin('/home/hongqingde/devdata/workspace_gitmp/input/paper/block2_well_CM/points3D_dense.ply')
+    # images_clear_tracks('/home/hongqingde/devdata/workspace_gitmp/input/block2_sxfx_SBT/sparse_mvs/images.bin')
+    # get_mvs_time(
+    #     [
+    #         '2.79',
+    #         '122',
+    #         '1.952',
+    #         '79',
+    #         '2.34.864',
+    #         '3.10.514',
+    #         '3.14.654',
+    #         '6.49.276',
+    #         '48.807',
+    #         '1.48.147',
+    #         '17.47.805',
+    #         '9.141',
+    #         '580'
+    #     ]
+    # )

traj_auto/tools/sparse_based_fixCamParamsAndDepth.py
@@ -99,13 +99,15 @@ def create_gt_points3D_by_depth_multi(trainDir_, K_, outputDir_, depthLimits_=(4
     dc2wKeysList_ = list(dw2c_.keys())
     traverseStep_ = 10
     dc2wLen_ = len(dc2wKeysList_)
-    dc2wIdxLast_ = (dc2wLen_ // int(traverseStep_))*int(traverseStep_)
+    dc2wIdxLast_ = (dc2wLen_ // int(traverseStep_)) * int(traverseStep_)
 
+    depthLimitsSub_ = depthLimits_[:4]
     for dc2wIdx_ in range(0, dc2wLen_, traverseStep_):
-        dc2wEnd_ = dc2wIdx_+traverseStep_ if dc2wIdx_ != dc2wIdxLast_ else dc2wLen_
+        dc2wEnd_ = dc2wIdx_ + traverseStep_ if dc2wIdx_ != dc2wIdxLast_ else dc2wLen_
 
         with ProcessPoolExecutor() as executor:
-            futures_ = [executor.submit(process_image, id_, dw2c_[id_], depthDir_, imagesDir_, K_, depthLimits_) for id_
+            futures_ = [executor.submit(process_image, id_, dw2c_[id_], depthDir_, imagesDir_, K_, depthLimitsSub_) for
+                        id_
                         in dc2wKeysList_[dc2wIdx_:dc2wEnd_]]
             for future_ in as_completed(futures_):
                 resultColors_, resultPoint3ds_, resultNormals_, resultSobelScores_ = future_.result()
@@ -126,23 +128,80 @@ def create_gt_points3D_by_depth_multi(trainDir_, K_, outputDir_, depthLimits_=(4
     pointCloud_.points = o3d.utility.Vector3dVector(point3ds_)
     pointCloud_.colors = o3d.utility.Vector3dVector(colors_)
     pointCloud_.normals = o3d.utility.Vector3dVector(normals_)
-    if filterType_ == 0:
-        points_ = np.hstack([np.asarray(point3ds_), np.asarray(colors_)])
-        pcdDown_ = pointCloud_
-    else:
-        if filterType_ == 1:
-            downVoxelSize_ = 1
-            pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSize_)
-        elif filterType_ == 2:
-            pcdDown_ = voxel_random_filter(pointCloud_, 0.1)
-        elif filterType_ == 3:
-            randomIdx_, randomIdxTexture_ = voxel_texture_filter(pointCloud_, sobelScores_, 0.2)
-            # pcdDown_ = pointCloud_.select_by_index(randomIdx_)
-            pcdDown_ = pointCloud_.select_by_index(randomIdxTexture_)
-            # print(f"dst num1 {len(pcdDown_.points)}, dst num2 {len(pcdDownTexture_.points)}")
-        else:
+
+    ptLimitMin_, ptLimitMax_ = depthLimits_[4], depthLimits_[5]
+
+    if ptLimitMin_ != -1 and ptLimitMax_ != -1 and ptLimitMin_ < ptLimitMax_:
+        if ptLimitMin_ <= point3ds_.shape[0] <= ptLimitMax_:
+            pass
+        elif point3ds_.shape[0] < ptLimitMin_:
             raise Exception
-        points_ = np.hstack([np.asarray(pcdDown_.points), np.asarray(pcdDown_.colors)])
+        else:
+            downVoxelSizeL_, downVoxelSizeR_ = 0.01, 0.5
+            downVoxelSizeM_ = -1
+            canFlg = True
+            while True:
+                pcdDownMin_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeR_)
+                ptNumsMin_ = np.array(pcdDownMin_.points).shape[0]
+                if ptNumsMin_ > ptLimitMax_:
+                    if downVoxelSizeR_ < 1.:
+                        downVoxelSizeR_ *= 2
+                    else:
+                        print('point clouds are so more')
+                        canFlg = False
+                        break
+                else:
+                    break
+
+            while True:
+                pcdDownMax_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeL_)
+                ptNumsMax_ = np.array(pcdDownMax_.points).shape[0]
+                if ptNumsMax_ < ptLimitMin_:
+                    if downVoxelSizeL_ > 0.01:
+                        downVoxelSizeL_ /= 2.
+                    else:
+                        print('point clouds are so sparse')
+                        canFlg = False
+                        break
+                else:
+                    break
+
+            if not canFlg:
+                raise Exception
+
+            while downVoxelSizeL_ < downVoxelSizeR_:
+                downVoxelSizeM_ = (downVoxelSizeL_ + downVoxelSizeR_) / 2.
+                pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeM_)
+                ptNumsT_ = np.array(pcdDown_.points).shape[0]
+
+                if ptLimitMin_ <= ptNumsT_ <= ptLimitMax_:
+                    break
+
+                if ptNumsT_ < ptLimitMin_:
+                    downVoxelSizeR_ = downVoxelSizeM_
+                else:
+                    downVoxelSizeL_ = downVoxelSizeM_
+
+            pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSizeM_)
+            points_ = np.hstack([np.asarray(pcdDown_.points), np.asarray(pcdDown_.colors)])
+    else:
+        if filterType_ == 0:
+            points_ = np.hstack([np.asarray(point3ds_), np.asarray(colors_)])
+            pcdDown_ = pointCloud_
+        else:
+            if filterType_ == 1:
+                downVoxelSize_ = 0.5
+                pcdDown_ = pointCloud_.voxel_down_sample(voxel_size=downVoxelSize_)
+            elif filterType_ == 2:
+                pcdDown_ = voxel_random_filter(pointCloud_, 0.1)
+            elif filterType_ == 3:
+                randomIdx_, randomIdxTexture_ = voxel_texture_filter(pointCloud_, sobelScores_, 0.2)
+                # pcdDown_ = pointCloud_.select_by_index(randomIdx_)
+                pcdDown_ = pointCloud_.select_by_index(randomIdxTexture_)
+                # print(f"dst num1 {len(pcdDown_.points)}, dst num2 {len(pcdDownTexture_.points)}")
+            else:
+                raise Exception
+            points_ = np.hstack([np.asarray(pcdDown_.points), np.asarray(pcdDown_.colors)])
 
     outputPath_ = os.path.join(outputDir_, fileName_)
     ext_ = fileName_.split('.')[-1]

traj_auto/tools/test.py
@@ -12,30 +12,35 @@
 #     cv2.waitKey()
 
 
-import cv2
 import numpy as np
+from scipy import interpolate
+
 import matplotlib.pyplot as plt
 
-# 1. 读取图像
-image = cv2.imread('/home/hongqingde/Downloads/DJI_0110.JPG')  # 替换为你的图像路径
-# 将 BGR 转换为 RGB
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#x = np.arange(0, 2*np.pi+np.pi/4, 2*np.pi/8)
+#y = np.sin(x)
 
-# 2. 计算直方图
-# 使用 cv2.calcHist() 计算每个通道的直方图
-channels = ('r', 'g', 'b')
-hist_data = {}
-for i, color in enumerate(channels):
-    hist_data[color] = cv2.calcHist([image], [i], None, [256], [0, 256])
+ctr =np.array( [
+                (3 , 1), (2.5, 4), (0, 1),
+                (-2.5, 4),
+                # (-3, 0), (-2.5, -4), (0, -1), (2.5, -4), (3, -1)
+                ])
 
-# 3. 绘制直方图
-plt.figure(figsize=(10, 5))
-for color, hist in hist_data.items():
-    plt.plot(hist, color=color)
-    plt.xlim([0, 256])
+x=ctr[:,0]
+y=ctr[:,1]
 
-plt.title('Color Histogram')
-plt.xlabel('Pixel Values')
-plt.ylabel('Frequency')
-plt.legend(channels)
+#x=np.append(x,x[0])
+#y=np.append(y,y[0])
+
+nums_ = 2*ctr.shape[0]
+tck, u = interpolate.splprep([x, y], k=3,s=0)
+u=np.linspace(0,1,num=nums_,endpoint=True)
+out = interpolate.splev(u,tck)
+
+plt.figure()
+plt.plot(x, y, 'ro', out[0], out[1], 'b')
+plt.legend(['Points', '插值B样条', '真实数据'],loc='best')
+plt.axis([min(x)-1, max(x)+1, min(y)-1, max(y)+1])
+plt.title('B样条插值')
 plt.show()
+

traj_auto/tools/utils.py
@@ -7,6 +7,7 @@ import math
 import os
 import matplotlib.pyplot as plt
 import sys
+from scipy import interpolate
 
 sys.path.append("./")
 
@@ -58,6 +59,8 @@ def line_traj_2D(psxy, pexy, residual, step, refineStepRatio=1):
         for i in range(accumLen):
             traj[i][0] += psxy[0]
             traj[i][1] += psxy[1]
+    else:
+        residualArc += residual
 
     return traj, residualArc
 
@@ -117,12 +120,13 @@ def get_Tw2c_1(t, R, isTranspose=False):
     return (-np.dot(rotM, t)).reshape((3,))
 
 
-def get_traj_by_node_sim(nodeList, step, rratio, yawType=-2, isClose=False):
+def get_traj_by_node_sim(nodeList, step, rratio, yawType=-2, isSmooth=True, isClose=False):
     """
     nodeList: 航线节点
     step: 航向间隔
     rstep: 拐弯处的航向间隔
     yawType: 偏航角类型，1-相机朝向与飞机行进方向一致，2-相机镜头朝着行进方向逆时针90度的方向，0-相机镜头朝着行进方向顺时针90度的方向，-2-大疆的行进方向, -1-五向飞行, -3-带偏移五向飞行, -4-五向飞行2
+    isSmooth: 是否使用b样条插值法对拐角处进行平滑
     """
     traj_ = []
     rest_ = 0
@@ -131,11 +135,126 @@ def get_traj_by_node_sim(nodeList, step, rratio, yawType=-2, isClose=False):
     lineds = []  # 每一段直线或弧线所包含的采集位置数目
     nodeLen = len(nodeList)
     travLen = nodeLen if isClose else nodeLen-1
-    for i in range(travLen):
-        j = (i+1) % nodeLen
-        trajt_, rest_ = line_traj_2D(nodeList[i], nodeList[j], rest_, step, rratio)
-        traj_ += trajt_
-        lineds.append(len(trajt_))
+
+    if not isSmooth:
+        for i in range(travLen):
+            j = (i+1) % nodeLen
+            trajt_, rest_ = line_traj_2D(nodeList[i], nodeList[j], rest_, step, rratio)
+            traj_ += trajt_
+            lineds.append(len(trajt_))
+    else:
+        rest_ = 0
+        pSrc_ = None
+        assert travLen % 2 == 0, "目前平滑操作仅适用于标准的牛耕路线，而标准的牛耕路线中轨迹节点有且仅有偶数个"
+        for i in range(0, travLen, 2):
+            j = (i+1) % nodeLen
+            if not pSrc_:
+                pSrc_ = nodeList[i]
+            pSrcT_ = nodeList[i]
+            pDst_ = nodeList[j]
+            trajt_, _ = line_traj_2D(pSrc_, pDst_, 0, step, 1)
+            traj_ += trajt_
+
+            if i <= (travLen-4):  # 只有这种情况下的拐角才有处理的必要
+                ctrPts_ = [[]]*4  # b样条插值所需的控制点
+                ctrPts_[1] = pDst_
+                ctrPts_[2] = nodeList[(i+2) % nodeLen]
+                samplePt3T = nodeList[(i+3) % nodeLen]
+                dis00 = gtls.distance_p1p2(pSrcT_, ctrPts_[1])
+                dis10 = gtls.distance_p1p2(ctrPts_[2], samplePt3T)
+                refineStep_ = step * rratio
+                if (dis00 > 0) and (dis10 > 0):  # b样条插值的控制点钟不能有重复点
+                    _ = traj_.pop()
+                    ctrPts_[0] = traj_[-1]
+                    dis01 = gtls.distance_p1p2(ctrPts_[0], ctrPts_[1])
+                    disR = dis01 / dis00
+                    ctrPts_[3] = [
+                        ctrPts_[2][0] + (samplePt3T[0]-ctrPts_[2][0])*disR,
+                        ctrPts_[2][1] + (samplePt3T[1]-ctrPts_[2][1])*disR
+                    ]
+
+                    ctrPts_ = np.array(ctrPts_)
+                    ctrXs_ = ctrPts_[:, 0]
+                    ctrYs_ = ctrPts_[:, 1]
+
+                    tckNodes_, _ = interpolate.splprep([ctrXs_, ctrYs_], k=3, s=0)
+                    tList_ = np.linspace(0, 1, num=12, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    disSum_ = 0
+                    for tIdx_ in range(11):
+                        disSum_ += gtls.distance_p1p2(
+                            [out_[0][tIdx_], out_[1][tIdx_]],
+                            [out_[0][tIdx_+1], out_[1][tIdx_+1]]
+                        )
+                    ptsNum_ = math.ceil(disSum_ / refineStep_)
+                    print("xxxxxxxxxxxxxxxxxxxxxxx==", ptsNum_, "==xxxxxxxxxxxxxxxxxxxxxxx")
+                    tList_ = np.linspace(0, 1, num=ptsNum_, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    wrapTrajs_ = [[out_[0][oIdx_], out_[1][oIdx_]] for oIdx_ in range(ptsNum_)]
+
+                    pSrc_ = wrapTrajs_.pop()
+                    traj_ += wrapTrajs_
+                elif (dis00 > 0) and (dis10 == 0):
+                    _ = traj_.pop()
+                    ctrPts_[0] = traj_[-1]
+                    _ = ctrPts_.pop()
+                    ctrPts_ = np.array(ctrPts_)
+                    ctrXs_ = ctrPts_[:, 0]
+                    ctrYs_ = ctrPts_[:, 1]
+
+                    tckNodes_, _ = interpolate.splprep([ctrXs_, ctrYs_], k=2, s=0)
+                    tList_ = np.linspace(0, 1, num=9, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    disSum_ = 0
+                    for tIdx_ in range(9):
+                        disSum_ += gtls.distance_p1p2(
+                            [out_[0][tIdx_], out_[1][tIdx_]],
+                            [out_[0][tIdx_ + 1], out_[1][tIdx_ + 1]]
+                        )
+                    ptsNum_ = math.ceil(disSum_ / refineStep_)
+                    print("xxxxxxxxxxxxxxxxxxxxxxx==", ptsNum_, "==xxxxxxxxxxxxxxxxxxxxxxx")
+                    tList_ = np.linspace(0, 1, num=ptsNum_, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    wrapTrajs_ = [[out_[0][oIdx_], out_[1][oIdx_]] for oIdx_ in range(ptsNum_)]
+                    pSrc_ = wrapTrajs_.pop()
+                    traj_ += wrapTrajs_
+                elif (dis00 == 0) and (dis10 > 0):
+                    _ = traj_.pop()
+                    ctrPts_[0] = traj_[-1]
+                    if dis10 < step:
+                        disR = 1
+                    else:
+                        disR = step / dis10
+                    ctrPts_[3] = [
+                        ctrPts_[2][0] + (samplePt3T[0] - ctrPts_[2][0]) * disR,
+                        ctrPts_[2][1] + (samplePt3T[1] - ctrPts_[2][1]) * disR
+                    ]
+
+                    ctrPts_ = np.array(ctrPts_)
+                    ctrXs_ = ctrPts_[:, 0]
+                    ctrYs_ = ctrPts_[:, 1]
+
+                    tckNodes_, _ = interpolate.splprep([ctrXs_, ctrYs_], k=3, s=0)
+                    tList_ = np.linspace(0, 1, num=12, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    disSum_ = 0
+                    for tIdx_ in range(11):
+                        disSum_ += gtls.distance_p1p2(
+                            [out_[0][tIdx_], out_[1][tIdx_]],
+                            [out_[0][tIdx_ + 1], out_[1][tIdx_ + 1]]
+                        )
+                    ptsNum_ = math.ceil(disSum_ / refineStep_)
+                    print("xxxxxxxxxxxxxxxxxxxxxxx==", ptsNum_, "==xxxxxxxxxxxxxxxxxxxxxxx")
+                    tList_ = np.linspace(0, 1, num=ptsNum_, endpoint=True)
+                    out_ = interpolate.splev(tList_, tckNodes_)
+                    wrapTrajs_ = [[out_[0][oIdx_], out_[1][oIdx_]] for oIdx_ in range(ptsNum_)]
+
+                    pSrc_ = wrapTrajs_.pop()
+                    traj_ += wrapTrajs_
+                else:
+                    trajt_, _ = line_traj_2D(pSrc_, ctrPts_[2], 0, step, 1)
+                    pSrc_ = trajt_.pop()
+                    traj_ += trajt_
 
     cot = 0
     # 0-行进方式为顺时针，1-行进方向为逆时针